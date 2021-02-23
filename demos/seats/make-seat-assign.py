8b9c72ab726efcdedf6dd75aefb7aca294cd760e
david <davidollodart@gmail.com>
2021-02-23 12:27:41 -0800

    added check by random sampling
    microstates (that is, all of the positions) are randomized to check if a
    solution. note however that randomly sampling some number of times never
    guarantees finding the optimum, and it doesn't give high confidence
    unless you do it enough times that you effectively sample the space,
    which removes the whole point of making a search routine to find the
    optimum. hence this is only a sanity check.

--- demos/seats/make-seat-assign.py
+++ demos/seats/make-seat-assign.py
@@ -3,6 +3,7 @@ import numpy as np
 from gradeanalytics import weighted_grade_matrix as wgm
 from conn import Seat, construct_seats
 from support import simulate
+from conn import eval_energy
 
 # wgm = wgm.loc['HW'] 
 wgm = wgm.groupby(level=0).agg(np.mean)
@@ -27,19 +28,33 @@ temp = diff_acc / len(es)**2
 print(f'temperature is {temp:.4f}')
 
 # assign student ids (circular arrangement)
-seats = [Seat(i, i + 1) for i in range(nstudents)]
-for s in seats:
-    s.adjs.extend([seats[(s.number + 1) % nstudents], seats[(s.number - 1) % nstudents]])
+#seats = [Seat(i, i + 1) for i in range(nstudents)]
+#for s in seats:
+#    s.adjs.extend([seats[(s.number + 1) % nstudents], seats[(s.number - 1) % nstudents]])
+#
+#seats = simulate(seats, penalty_func, kbt = temp)
+#for s in seats:
+#    print(s.number, s.sid, tuple((x.number, x.sid) for x in s.adjs))
 
-seats = simulate(seats, penalty_func, kbt = temp)
-for s in seats:
-    print(s.number, s.sid, tuple((x.number, x.sid) for x in s.adjs))
-
+# assign student ids (rectangular arrangement)
 seats = construct_seats(4,3)
 for s in seats:
     s.sid += 1
 
 print(f'sids {",".join(f"{x}" for x in range(nstudents+1, 3*4 + 1))} are empty')
-seats = simulate(seats, penalty_func, kbt=temp)
-for s in seats:
+sseats = simulate(seats, penalty_func, kbt=temp)
+for s in sseats:
     print(s.number, s.sid, [(x.number, x.sid) for x in s.adjs])
+
+opt = eval_energy(sseats, penalty_func)
+x = np.array(range(12))
+x += 1
+for _ in range(1000):
+    np.random.shuffle(x)
+    for c, s in enumerate(seats):
+        s.sid = x[c]
+    e = eval_energy(seats, penalty_func)
+    de = e - opt
+    if de < 0:
+        print('found better solution in 1000 random samples'
+              '\nfix the temperature or other simulation parameters')
